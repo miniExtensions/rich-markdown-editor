39f2e0a4ea614823f2320d801b111e31ad094ce1
Jhon Idrovo <jhonidrovo3@gmail.com>
2023-07-11 17:49:20 -0500

    Added handler for click outside

--- src/components/CommandMenu.tsx
+++ src/components/CommandMenu.tsx
@@ -74,6 +74,7 @@ class CommandMenu<T = MenuItem> extends React.Component<Props<T>, State> {
   componentDidMount() {
     if (!SSR) {
       window.addEventListener("keydown", this.handleKeyDown);
+      window.addEventListener("click", this.handleClick);
     }
   }
 
@@ -102,6 +103,7 @@ class CommandMenu<T = MenuItem> extends React.Component<Props<T>, State> {
   componentWillUnmount() {
     if (!SSR) {
       window.removeEventListener("keydown", this.handleKeyDown);
+      window.removeEventListener("click", this.handleClick);
     }
   }
 
@@ -172,8 +174,17 @@ class CommandMenu<T = MenuItem> extends React.Component<Props<T>, State> {
       this.close();
     }
   };
+  /**
+   * If the click is outside the menu, close it
+   */
+  handleClick = (event: MouseEvent): void => {
+    if (!this.props.isActive) return;
 
-  insertItem = item => {
+    const target = event.target as HTMLElement;
+    if (target.id === "block-menu-container") return;
+    this.props.onClose();
+  };
+  insertItem = (item) => {
     switch (item.name) {
       case "image":
         return this.triggerImagePick();
@@ -254,11 +265,11 @@ class CommandMenu<T = MenuItem> extends React.Component<Props<T>, State> {
     }
   };
 
-  triggerLinkInput = item => {
+  triggerLinkInput = (item) => {
     this.setState({ insertItem: item });
   };
 
-  handleImagePicked = event => {
+  handleImagePicked = (event) => {
     const files = getDataTransferFiles(event);
 
     const {
@@ -269,7 +280,7 @@ class CommandMenu<T = MenuItem> extends React.Component<Props<T>, State> {
       onShowToast,
     } = this.props;
     const { state } = view;
-    const parent = findParentNode(node => !!node)(state.selection);
+    const parent = findParentNode((node) => !!node)(state.selection);
 
     this.clearSearch();
 
@@ -423,7 +434,7 @@ class CommandMenu<T = MenuItem> extends React.Component<Props<T>, State> {
       items = items.concat(embedItems);
     }
 
-    const filtered = items.filter(item => {
+    const filtered = items.filter((item) => {
       if (item.name === "separator") return true;
 
       // Some extensions may be disabled, remove corresponding menu items
@@ -536,7 +547,7 @@ const LinkInputWrapper = styled.div`
 const LinkInput = styled(Input)`
   height: 36px;
   width: 100%;
-  color: ${props => props.theme.blockToolbarText};
+  color: ${(props) => props.theme.blockToolbarText};
 `;
 
 const List = styled.ol`
@@ -555,7 +566,7 @@ const ListItem = styled.li`
 const Empty = styled.div`
   display: flex;
   align-items: center;
-  color: ${props => props.theme.textSecondary};
+  color: ${(props) => props.theme.textSecondary};
   font-weight: 500;
   font-size: 14px;
   height: 36px;
@@ -569,14 +580,14 @@ export const Wrapper = styled.div<{
   left?: number;
   isAbove: boolean;
 }>`
-  color: ${props => props.theme.text};
-  font-family: ${props => props.theme.fontFamily};
+  color: ${(props) => props.theme.text};
+  font-family: ${(props) => props.theme.fontFamily};
   position: absolute;
-  z-index: ${props => props.theme.zIndex + 100};
-  ${props => props.top !== undefined && `top: ${props.top}px`};
-  ${props => props.bottom !== undefined && `bottom: ${props.bottom}px`};
-  left: ${props => props.left}px;
-  background-color: ${props => props.theme.blockToolbarBackground};
+  z-index: ${(props) => props.theme.zIndex + 100};
+  ${(props) => props.top !== undefined && `top: ${props.top}px`};
+  ${(props) => props.bottom !== undefined && `bottom: ${props.bottom}px`};
+  left: ${(props) => props.left}px;
+  background-color: ${(props) => props.theme.blockToolbarBackground};
   border-radius: 4px;
   box-shadow: rgba(0, 0, 0, 0.05) 0px 0px 0px 1px,
     rgba(0, 0, 0, 0.08) 0px 4px 8px, rgba(0, 0, 0, 0.08) 0px 2px 4px;
@@ -601,7 +612,7 @@ export const Wrapper = styled.div<{
   hr {
     border: 0;
     height: 0;
-    border-top: 1px solid ${props => props.theme.blockToolbarDivider};
+    border-top: 1px solid ${(props) => props.theme.blockToolbarDivider};
   }
 
   ${({ active, isAbove }) =>
