5ad40d883dcd5fc7779dbb840d8e9223f25b5716
Jhon Idrovo <jhonidrovo3@gmail.com>
2023-07-12 12:06:30 -0500

    closing

--- src/plugins/BlockMenuTrigger.tsx
+++ src/plugins/BlockMenuTrigger.tsx
@@ -46,6 +46,7 @@ export default class BlockMenuTrigger extends Extension {
     const button = document.createElement("button");
     button.className = "block-menu-trigger";
     button.type = "button";
+    button.onblur = () => this.options.onClose();
     ReactDOM.render(<PlusIcon color="currentColor" />, button);
 
     return [
@@ -93,9 +94,9 @@ export default class BlockMenuTrigger extends Extension {
 
             return false;
           },
-          decorations: state => {
+          decorations: (state) => {
             const parent = findParentNode(
-              node => node.type.name === "paragraph"
+              (node) => node.type.name === "paragraph"
             )(state.selection);
 
             if (!parent) {
